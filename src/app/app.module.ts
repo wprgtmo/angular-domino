ca4db8d2b636e0234931f3f568f25e0cfb26501e
wilfredo <wilfredo.perez@nauta.cu>
2021-09-23 14:17:31 -0400

    Se crearon los componentes básicos de la app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { EventoComponent } from './common/components/evento/evento.component';
+import { JugadorComponent } from './common/components/jugador/jugador.component';
+import { LoginComponent } from './common/components/login/login.component';
+import { RegisterComponent } from './common/components/register/register.component';
+import { HeaderComponent } from './common/components/header/header.component';
+import { MenuComponent } from './common/components/menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EventoComponent,
+    JugadorComponent,
+    LoginComponent,
+    RegisterComponent,
+    HeaderComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/common/components/jugador/jugador.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-jugador',
+  templateUrl: './jugador.component.html',
+  styleUrls: ['./jugador.component.css']
+})
+export class JugadorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
