7194ffdaa28aa64e1d2d7bfedf44d03e10cad3d7
wilfredo <wilfredo.perez@nauta.cu>
2021-09-23 20:20:55 -0400

    Eliminados los componentes del app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,24 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { EventoComponent } from './common/components/evento/evento.component';
-import { JugadorComponent } from './common/components/jugador/jugador.component';
-import { LoginComponent } from './common/components/login/login.component';
-import { RegisterComponent } from './common/components/register/register.component';
-import { HeaderComponent } from './common/components/header/header.component';
-import { MenuComponent } from './common/components/menu/menu.component';
 import { AuthPagesModule } from './pages/auth-pages/auth-pages.module';
 import { DashboardPagesModule } from './pages/dashboard-pages/dashboard-pages.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    EventoComponent,
-    JugadorComponent,
-    LoginComponent,
-    RegisterComponent,
-    HeaderComponent,
-    MenuComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
