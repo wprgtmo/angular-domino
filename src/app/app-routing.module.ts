e50ba4acf46e245d7f0e91ec324ed69b1cc127f1
wilfredo <wilfredo.perez@nauta.cu>
2021-09-23 20:46:03 -0400

    Configuracion de la ruta del login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
+import { LoginPageComponent } from './pages/auth-pages/login-page/login-page.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginPageComponent
+  },
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
