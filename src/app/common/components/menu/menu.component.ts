f184337fd9c6c6cbde2c66cd8a6a497507f4adc8
wilfredo <wilfredo.perez@nauta.cu>
2021-11-17 08:29:24 -0500

    Se pasó el menu lateral para un compnente

--- src/app/common/components/menu/menu.component.ts
+++ src/app/common/components/menu/menu.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, AfterViewInit, ViewChild } from '@angular/core';
+import {
+  MatNavList,
+  MatSelectionList,
+  MatSelectionListChange,
+} from '@angular/material/list';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.css']
+  styleUrls: ['./menu.component.css'],
 })
-export class MenuComponent implements OnInit {
+export class MenuComponent {
+  opcionesMenu = [
+    {
+      nombre: 'Eventos',
+      enlace: 'eventsCard',
+      icono: 'event',
+      isSelected: true,
+    },
+    {
+      nombre: 'Jugadores',
+      enlace: 'jugadores',
+      icono: 'people_outline_white',
+      isSelected: false,
+    },
+    {
+      nombre: 'Estadisticas',
+      enlace: 'estadisticas',
+      icono: 'equalizer_white',
+      isSelected: false,
+    },
+  ];
 
-  constructor() { }
+  constructor(private ruta: Router){}
 
-  ngOnInit(): void {
+  onListSelectionChange(menu: MatSelectionListChange) {
+    this.ruta.navigateByUrl(menu.source.selectedOptions.selected[0]?.value);
   }
-
 }
